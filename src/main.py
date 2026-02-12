3960756ae5d404347e875eedd3f1f70e5e089548
nycelease2 <raaghav@thapliyal.net>
2026-02-12 11:54:05 +0530

    init commit

---
+++ src/main.py
@@ -0,0 +1,7 @@
+class session:
+    def __init__(self, start_time, end_time, title, desc):
+        self.start_time = start_time
+        self.end_time = end_time
+        self.title = title
+        self.desc = desc
+        
